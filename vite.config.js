d9080bd1e3c9d9e5ee2f9c743836619a9d447a42
rezowan-miya <mdrezowanmiya6@gmail.com>
2025-10-20 18:14:39 +0600

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/React-and-Tailwind-Css-Project/', // তোমার GitHub repo name
 })
+
